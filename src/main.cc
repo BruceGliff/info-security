8b629bae732dd6cce05d34664f01d282e311d9b7
leokondrashov <leo.kondrashov@gmail.com>
2021-11-16 05:53:40 -0500

    channel selecting works

--- src/main.cc
+++ src/main.cc
@@ -9,10 +9,11 @@ int main(int argc, char *argv[]) {
 
   path bin_path = path{argv[0]}.errase_filename();
 
+
+
   Monitor m{};
   AP_Selecter ap{m.GetIface()};
   AP_info_tiny apt {ap.ChooseAP().GetPreferedAP()};
-  apt.channel = 11;
   ST_Scanner st{bin_path, apt, m.GetIface()};
 
   return 0;
